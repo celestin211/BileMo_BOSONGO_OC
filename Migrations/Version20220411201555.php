00abc2e4ee02d34ce1750a7994342b13590758f6
BOSONGO <celestin.mombela@yahoo.fr>
2022-04-20 23:46:54 +0200

    Migration of all table
    Migration

--- Migrations/Version20220411201555.php
+++ Migrations/Version20220411201555.php
@@ -22,11 +22,11 @@ final class Version20220411201555 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE client (id INT AUTO_INCREMENT NOT NULL, user_client_id INT DEFAULT NULL, email VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, INDEX IDX_C7440455190BE4C5 (user_client_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE person (id INT AUTO_INCREMENT NOT NULL, user_client_id INT DEFAULT NULL, email VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, INDEX IDX_C7440455190BE4C5 (user_client_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, user_client_id INT DEFAULT NULL, user_id INT DEFAULT NULL, brand VARCHAR(255) NOT NULL, color VARCHAR(255) NOT NULL, model VARCHAR(255) NOT NULL, release_year INT NOT NULL, screen_size DOUBLE PRECISION NOT NULL, memory_gb INT NOT NULL, megapixels INT NOT NULL, price DOUBLE PRECISION NOT NULL, INDEX IDX_D34A04AD190BE4C5 (user_client_id), INDEX IDX_D34A04ADA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE client ADD CONSTRAINT FK_C7440455190BE4C5 FOREIGN KEY (user_client_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD190BE4C5 FOREIGN KEY (user_client_id) REFERENCES client (id)');
+        $this->addSql('ALTER TABLE person ADD CONSTRAINT FK_C7440455190BE4C5 FOREIGN KEY (user_client_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD190BE4C5 FOREIGN KEY (user_client_id) REFERENCES person (id)');
         $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04ADA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
     }
 
@@ -36,9 +36,9 @@ final class Version20220411201555 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE product DROP FOREIGN KEY FK_D34A04AD190BE4C5');
-        $this->addSql('ALTER TABLE client DROP FOREIGN KEY FK_C7440455190BE4C5');
+        $this->addSql('ALTER TABLE person DROP FOREIGN KEY FK_C7440455190BE4C5');
         $this->addSql('ALTER TABLE product DROP FOREIGN KEY FK_D34A04ADA76ED395');
-        $this->addSql('DROP TABLE client');
+        $this->addSql('DROP TABLE person');
         $this->addSql('DROP TABLE product');
         $this->addSql('DROP TABLE user');
     }
