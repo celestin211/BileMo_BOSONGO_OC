c09c55aaa75ce038e8e914fd645bc7870dd5c5e9
BOSONGO <celestin.mombela@yahoo.fr>
2022-04-20 20:09:56 +0200

    Update and rename ListOfPersonController.php to ListOfPeopleController.php

--- Controller/ListOfPeopleController.php
+++ Controller/ListOfPeopleController.php
@@ -2,40 +2,40 @@
 
 namespace App\Controller;
 
-use App\DTO\ClientDTO;
-use App\Links\LinksClientDTOGenerator;
-use App\Paging\ClientPaging;
+use App\DTO\PersonDTO;
+use App\Links\LinksPersonDTOGenerator;
+use App\Paging\PeoplePaging;
 use App\Responder\JsonResponder;
 use Nelmio\ApiDocBundle\Annotation\Security as SecurityDoc;
 use Swagger\Annotations as SWG;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ListOfClientController
+class ListOfPeopleController
 {
     private $responder;
     private $paging;
-    private $clientDTO;
+    private $personDTO;
     private $links;
 
     public function __construct(
         JsonResponder $responder,
-        ClientPaging $paging,
-        ClientDTO $clientDTO,
-        LinksClientDTOGenerator $links
+        PeoplePaging $paging,
+        PersonDTO $personDTO,
+        LinksPersonDTOGenerator $links
     ) {
         $this->responder = $responder;
         $this->paging = $paging;
-        $this->clientDTO = $clientDTO;
+        $this->personDTO = $personDTO;
         $this->links = $links;
     }
 
     /**
-     * @Route("/client", methods={"GET"}, name="ListOfClient")
+     * @Route("/people", methods={"GET"}, name="listOfPeople")
      *
      * @SWG\Response(
      *     response=200,
-     *     description="Return list of client",
+     *     description="Return list of people",
      *
      * )
      * @SWG\Response(
@@ -46,19 +46,19 @@ class ListOfClientController
      *     name="page",
      *     in="query",
      *     type="integer",
-     *     description="Client pagination"
+     *     description="People pagination"
      * )
-     * @SWG\Tag(name="Client")
+     * @SWG\Tag(name="Person")
      * @SecurityDoc(name="Bearer")
      */
-    public function ListOfClient(Request $request)
+    public function listOfpeople(Request $request)
     {
-        $client = $this->paging->getDatas($request->query->get('page'));
+        $people = $this->paging->getDatas($request->query->get('page'));
 
-        $clientDTO = $this->clientDTO->getClientDTO($client);
+        $peopleDTO = $this->personDTO->getPeopleDTO($people);
 
-        $this->links->addLinks($clientDTO);
+        $this->links->addLinks($peopleDTO);
 
-        return $this->responder->send($request, $clientDTO);
+        return $this->responder->send($request, $peopleDTO);
     }
 }
