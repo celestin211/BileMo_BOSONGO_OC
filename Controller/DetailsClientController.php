cd90ad86d761a9228eb9579d218d593b13e2173d
BOSONGO <celestin.mombela@yahoo.fr>
2022-04-20 20:04:56 +0200

    Delete of person on field
    Absoluty have a token to delete a person on fields

--- Controller/DetailsClientController.php
+++ Controller/DetailsClientController.php
@@ -2,41 +2,41 @@
 
 namespace App\Controller;
 
-use App\DTO\ClientDTO;
+use App\DTO\PersonDTO;
 use App\Exceptions\ApiException;
-use App\Links\LinksClientDTOGenerator;
-use App\Repository\ClientRepository;
+use App\Links\LinksPersonDTOGenerator;
+use App\Repository\PersonRepository;
 use App\Responder\JsonResponder;
-use App\Security\Voter\ClientVoter;
+use App\Security\Voter\PersonVoter;
 use Nelmio\ApiDocBundle\Annotation\Security as SecurityDoc;
 use Swagger\Annotations as SWG;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
 
-class DetailsClientController
+class DetailsPersonController
 {
-    private $clientRepository;
-    private $clientVoter;
+    private $personRepository;
+    private $personVoter;
     private $security;
     private $responder;
 
     public function __construct(
-        ClientRepository $clientRepository,
-        ClientVoter $clientVoter,
+        PersonRepository $personRepository,
+        PersonVoter $personVoter,
         Security $security,
         JsonResponder $responder,
-        LinksClientDTOGenerator $links
+        LinksPersonDTOGenerator $links
     ) {
-        $this->clientRepository =$clientRepository;
-        $this->clientVoter = $clientVoter;
+        $this->personRepository = $personRepository;
+        $this->personVoter = $personVoter;
         $this->security = $security;
         $this->responder = $responder;
         $this->links = $links;
     }
 
     /**
-     * @Route("/client/{id}", methods={"GET"}, name="detailsClient")
+     * @Route("/person/{id}", methods={"GET"}, name="detailsPerson")
      * @SWG\Response(
      *     response=200,
      *     description="Returns one product",
@@ -56,24 +56,30 @@ class DetailsClientController
      *     type="integer",
      *     description="The id of the person"
      * )
-     * @SWG\Tag(name="Client")
+     * @SWG\Tag(name="People")
      * @SecurityDoc(name="Bearer")
      */
-    public function detailsClient($id, Request $request)
-    {
-        $client = $this->personRepository->findOneById($id);
-        if (null == $person) {
-            throw new ApiException('This person not exist.', 404);
-        }
+     public function detailsPerson($id, Request $request)
+     {
+         /*Check person by his id on the field */
+         
+       $person = $this->personRepository->findOneById($id);
+   
+       if (null == $person) {
+           throw new ApiException('This person not exist.', 404);
+       }
+         
+        /** Absoluty have a token to delete a person on fields  **/
+       $vote = $this->personVoter->vote($this->security->getToken(), $person, ['view']);
+         
+        /* Make sure the token cant be empty on fields else throw exception error 403 RichardSon level */
+       if ($vote < 1) {
+           throw new ApiException('You are not authorized to access this resource.', 403);
+       }
+       
+       $personDTO = new PersonDTO($person);
+       $this->links->addLinks($personDTO);
 
-        $vote = $this->clientVoter->vote($this->security->getToken(), $client, ['view']);
-        if ($vote < 1) {
-            throw new ApiException('You are not authorized to access this resource.', 403);
-        }
-
-        $clientDTO = new ClientDTO($client);
-        $this->links->addLinks($clientDTO);
-
-        return $this->responder->send($request, $clientDTO);
-    }
+       return $this->responder->send($request, $personDTO);
+   }
 }
