04626e2cc7f8167c3b44bf3b917c2d99b0091954
BOSONGO <celestin.mombela@yahoo.fr>
2022-04-20 20:43:56 +0200

    Generator of router on the fields
    Generator of router on the fields

--- Links/LinksProductDTOGenerator.php
+++ Links/LinksProductDTOGenerator.php
@@ -6,6 +6,7 @@ class LinksProductDTOGenerator extends LinksGenerator
 {
     protected function createLinks(object $object)
     {
+/* générateur de routage * /
         $object->set_links(['self' => $this->urlGenerator->generate('detailsProduct', ['id' => $object->getId()], 0),
                             'list' => $this->urlGenerator->generate('listOfProducts', [], 0),
                             'add' => $this->urlGenerator->generate('addProduct', [], 0),
