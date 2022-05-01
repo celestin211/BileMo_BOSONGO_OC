bfeb12a54eb3d98fc68ff498cf46ce8b34838eeb
BOSONGO <celestin.mombela@yahoo.fr>
2022-05-01 18:22:42 +0200

    Update UserPaging.php

--- Paging/UserPaging.php
+++ Paging/UserPaging.php
@@ -3,24 +3,22 @@
 namespace App\Paging;
 
 use App\Exceptions\ApiException;
-//use App\Repository\UserRepository;
+use App\Repository\UserRepository;
 
 class UserPaging
 {
-    
-    /** limited of 5 pages **/
-    private const NB_USERS_PAGED = 5;
+    private const NB_PRODUCTS_PAGED = 5;
 
     private $userRepository;
     private $nbUsers;
     private $maxPages;
 
     public function __construct(
-  //      UserRepository $userRepository
+        UserRepository $userRepository
     ) {
         $this->userRepository = $userRepository;
         $this->nbUsers = $this->userRepository->count([]);
-        $this->maxPages = intval(ceil($this->nbUsers / self::NB_USERS_PAGED));
+        $this->maxPages = intval(ceil($this->nbUsers / self::NB_PRODUCTS_PAGED));
     }
 
     public function getDatas($page)
@@ -33,8 +31,8 @@ class UserPaging
             throw new ApiException('The page must be between 1 and '.$this->maxPages.'.', 404);
         }
 
-        $offset = self::NB_USERS_PAGED * ($page - 1);
+        $offset = self::NB_PRODUCTS_PAGED * ($page - 1);
 
-        return $users = $this->userRepository->findBy([], [], self::NB_USERS_PAGED, $offset);
+        return $users = $this->userRepository->findBy([], [], self::NB_PRODUCTS_PAGED, $offset);
     }
 }
